595b5a68c22641679eaef58a336ac915fd0a3abd
vanessascarin <vanessa.hb.scarin@gmail.com>
2025-03-10 10:39:35 -0300

    Zero Values

---
+++ Módulo_4/Zerovalues.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Bool: %v\n", b)
+	fmt.Printf("String: %v\n", s)
+
+	i = 3
+	fmt.Printf("Inteiro: %v\n", i)
+
+}
