b339668a498a7b49e26793e39a24ef7a12323881
vanessascarin <vanessa.hb.scarin@gmail.com>
2025-03-10 08:13:37 -0300

    Tipos em Go

---
+++ Módulo_4/Tipos.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+	fmt.Printf("Type: %T - Value: %v\n", "vanessa", "vanessa")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+}
