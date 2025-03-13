62a05c81d4b0d54a9ea19099c4663ca25b758302
vanessascarin <vanessa.hb.scarin@gmail.com>
2025-03-13 11:15:38 -0300

    Pacotes externos, comandos básicos do go.mod e funções públicas e privadas

--- Módulo_8/main.go
+++ Módulo_8/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"calculator/math" //nome_módulo/nome_pacote
 	"fmt"
+
+	"github.com/labstack/echo/v4" //porque usamos uma função de uma biblioteca externa
+	// para externo, usar o go mod tidy e ele vai fazer o download
+	//também cria automaticamente o arquivo go.sum, NUNCA ALTERAR ELE
 )
 
 func main() {
+
+	// Echo instance
+	e := echo.New()
+
 	sum := math.Sum(1, 2)
 	fmt.Println(sum)
 
-	sub := math.Sub(1, 2)
+	sub := math.sub(1, 2) //sub não vai funcionar pq a função começa com letra minúscula, ou seja, é privada
 	fmt.Println(sub)
 }
--- Módulo_8/math/math.go
+++ Módulo_8/math/math.go
@@ -1,9 +1,15 @@
-package math
+package math //pode ter o mesmo nome que o arquivo ou não
 
 func Sum(a, b int) int {
 	return a + b
 }
 
-func Sub(a, b int) int {
+func sub(a, b int) int {
 	return a - b
 }
+
+//usar o go mod init e um "identificador do módulo", por
+// exemplo github.com/vanessascarin/calculator, se eu
+// tivesse um projeto no github com esse path
+// NESSE CASO, usamos go mod init calculator, e ele cria o arquivo
+// go.mod no mesmo diretório que o código do pacote
